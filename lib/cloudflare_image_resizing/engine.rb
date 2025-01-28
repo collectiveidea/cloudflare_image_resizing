3f7967be79927b87fbcc6d6328c5114f33bb902e
Daniel Morrison <daniel@collectiveidea.com>
2025-01-28 18:45:01 -0500

    Specify engine_name
    This helps the config work as expected.

--- lib/cloudflare_image_resizing/engine.rb
+++ lib/cloudflare_image_resizing/engine.rb
@@ -1,6 +1,7 @@
 module CloudflareImageResizing
   class Engine < ::Rails::Engine
     isolate_namespace CloudflareImageResizing
+    engine_name "cloudflare_image_resizing"
 
     config.cloudflare_image_resizing = Configuration.new
 
